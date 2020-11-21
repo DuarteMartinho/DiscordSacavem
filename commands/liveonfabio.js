4cfa8e6488578aa89cffc6598834f46b665f74f2
DuarteMartinho <duartemartinho11@gmail.com>
2020-11-21 22:11:07 +0000

    Fix nos comandos

--- commands/liveonfabio.js
+++ commands/liveonfabio.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 module.exports = {
 	name: 'liveonfabio',
-	description: 'liveonfabio!',
+	description: 'live do fabio!',
 	execute(message, args, bot) {
         message.delete()
-        message.channel.send(`<@&777918294761668628> \n O Fabio esta agora em live na Twitch. Vao la dar o vosso apoio nao custa nada. \n https://twitch.tv/fabiogarrido97 \n @everyone`)
+        message.channel.send(`<@&777928493996048435> \n O Fabio esta agora em live na Twitch. Vao la dar o vosso apoio nao custa nada. \n https://twitch.tv/fabiogarrido97 \n @everyone`)
         
     },
 };
--- commands/liveonserrano.js
+++ commands/liveonserrano.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 
 module.exports = {
 	name: 'liveonserrano',
-	description: 'liveonserrano!',
+	description: 'live do serrano!',
 	execute(message, args, bot) {
         message.delete()
-        message.channel.send(`<@&777900178119458866> \n O ElSerrano esta agora em live na Twitch. Vao la dar o vosso apoio nao custa nada. \n https://twitch.tv/elserrano97 \n @everyone`)
+        message.channel.send(`<@&777928493996048435> \n O ElSerrano esta agora em live na Twitch. Vao la dar o vosso apoio nao custa nada. \n https://twitch.tv/elserrano97 \n @everyone`)
     },
 };
